ce788152c5dd602f9ea7a464760ecf2611078015
DKDI <tfilipczuk4@gmail.com>
2024-11-17 01:21:49 +0100

    Update KOSZ.ino

--- KOSZ.ino
+++ KOSZ.ino
@@ -1,12 +1,28 @@
 #include "esp_camera.h"
 #include <WiFi.h>
-#include <WiFiClient.h>
 #include <Stepper.h>
 
 #define CAMERA_MODEL_AI_THINKER 
-#include "camera_pins.h"
 
+#define PWDN_GPIO_NUM  32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM  0
+#define SIOD_GPIO_NUM  26
+#define SIOC_GPIO_NUM  27
 
+#define Y9_GPIO_NUM    35
+#define Y8_GPIO_NUM    34
+#define Y7_GPIO_NUM    39
+#define Y6_GPIO_NUM    36
+#define Y5_GPIO_NUM    21
+#define Y4_GPIO_NUM    19
+#define Y3_GPIO_NUM    18
+#define Y2_GPIO_NUM    5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM  23
+#define PCLK_GPIO_NUM  22
+
+#define LED_GPIO_NUM   4
 
 int motorSpeed = 10;
 Stepper myStepper(2048,15,14,2,4);
@@ -31,8 +47,6 @@ void setup() {
 
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);  
-  pinMode(motorPin, OUTPUT);
-  digitalWrite(motorPin, LOW); 
 
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
